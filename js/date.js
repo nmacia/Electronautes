94747cb17622dc7b4da616c75d4a14504b24ce5c
Juanita Devis <ladevis@gmail.com>
2017-04-25 13:56:51 +0200

    weatherGraphics properties, codes and images Check comments and slide 44

--- js/date.js
+++ js/date.js
@@ -11,6 +11,8 @@
 var tday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var tmonth = ['January','February','March','April','May','June','July','August','September','October','November','December']; 
 
+
+
 // Retrieve date.
 window.onload = function getClock(){
   var today = new Date();
@@ -100,57 +102,147 @@ function writeUserData(mood, energy, temperature, noise, password) {
   });
 }
 
-function changeBackground ( weatherCode ) {
+//function changeBackground ( weatherCode ) {
+//
+//  var color = "#38BFB3";
+//
+//  switch ( weatherCode ) {
+//    //Rain showers 10, 11,12, 40 color #3E6A74
+//    case '10': case '11': case '12': case '40': 
+//            color = "#3E6A74"
+//            break;
+//    //Sunny 32, 34 #FDCF07
+//    case '32': case '34':
+//            color = "#FDCF07"
+//            break;
+//    //Snow 13, 14, 15, 16, 41,42, 43, 46 color #CCCEDA
+//    case '13': case '14': case '15': case '16': case '41': case '42': case '43': case '46':
+//            color = "#CCCEDA"
+//            break;     
+//    //Tunderstorms 1, 3, 4,37, 38, 39, 45, 47 color #445197
+//    case '1': case '3': case '4': case '37': case '38': case '39': case '45': case '47':
+//            color = "#445197"
+//            break; 
+//    //Hail 17, 35, #517897 #6b6b6b
+//     case '17': case '35':
+//            color = "#6b6b6b"
+//            break;
+//    //Foggy 20 #7A7A70
+//      case '20':
+//            color = "#6b6b6b"
+//            break;
+//    //Haze 21 #F4AD2F
+//     case '21':
+//            color = "#F59117"
+//            break;
+//    //Cold  25 #8EB1C4
+//    case '25':
+//            color = "#8EB1C4"
+//            break;
+//    //Hot 36 #F56544
+//          case '36':
+//            color = "#F56544"
+//            break;
+//    //Cloudy 26, 27, 44 #A4A09D
+//          case '26': case '27': case '44':
+//            color = "#A4A09D"
+//            break;
+//    //No asociation 0, 2, 3200
+//    case '30' : color = "#ffcc00"
+//              break;
+//    
+//  }
+//
+//  document.body.style.background = color;
+// 
+// }
 
-  var color = "#38BFB3";
-
-  switch ( weatherCode ) {
-    //Rain showers 10, 11,12, 40 color #3E6A74
-    case '10': case '11': case '12': case '40': 
-            color = "#3E6A74"
-            break;
-    //Sunny 32, 34 #FDCF07
-    case '32': case '34':
-            color = "#FDCF07"
-            break;
-    //Snow 13, 14, 15, 16, 41,42, 43, 46 color #CCCEDA
-    case '13': case '14': case '15': case '16': case '41': case '42': case '43': case '46':
-            color = "#CCCEDA"
-            break;     
-    //Tunderstorms 1, 3, 4,37, 38, 39, 45, 47 color #445197
-    case '1': case '3': case '4': case '37': case '38': case '39': case '45': case '47':
-            color = "#445197"
-            break; 
-    //Hail 17, 35, #517897 #6b6b6b
-     case '17': case '35':
-            color = "#6b6b6b"
-            break;
-    //Foggy 20 #7A7A70
-      case '20':
-            color = "#6b6b6b"
-            break;
-    //Haze 21 #F4AD2F
-     case '21':
-            color = "#F59117"
-            break;
-    //Cold  25 #8EB1C4
-    case '25':
-            color = "#8EB1C4"
-            break;
-    //Hot 36 #F56544
-          case '36':
-            color = "#F56544"
-            break;
-    //Cludy 26, 27, 44 
-    //No asociation 0, 2, 3200
-    case '30' : color = "#ffcc00"
-              break;
+// returns the graphical parameters (for now background color and image filename) of a given weather code
+function getWeatherGraphicsParams( weatherCode ) {  
+    // TODO: if previously computed weatherParams are cached, performance can be improved
     
-  }
+    var weatherGraphics = { };
+    var color = "#38BFB3";
+    var img = ""; // what is the default image?
+    
+    switch ( weatherCode ) {
+        //Rain showers 10, 11,12, 40 color #3E6A74
+        case '10': case '11': case '12': case '40': 
+                color = "#3E6A74";
+                img = "rain.png";
+                break;
+        //Sunny 32, 34 #FDCF07
+        case '32': case '34':
+                color = "#FDCF07";
+                img = "sunny.png";
+                break;
+        //Snow 13, 14, 15, 16, 41,42, 43, 46 color #CCCEDA
+        case '13': case '14': case '15': case '16': case '41': case '42': case '43': case '46':
+                color = "#CCCEDA";
+                img = "snow.png";
+                break;     
+        //Thunderstorms 1, 3, 4,37, 38, 39, 45, 47 color #445197
+        case '1': case '3': case '4': case '37': case '38': case '39': case '45': case '47':
+                color = "#445197";
+                img = "thunderstorms.png";
+                break; 
+        //Hail 17, 35, #517897 #6b6b6b
+         case '17': case '35':
+                color = "#6b6b6b";
+                img = "hail.png";
+                break;
+        //Foggy 20 #7A7A70
+          case '20':
+                color = "#6b6b6b";
+                img = "foggy.png";
+                break;
+        //Haze Smoky 21, 22 #F4AD2F
+         case '21': case '22':
+                color = "#F59117";
+                img = "haze.png";
+                break;
+        //Cold  25 #8EB1C4
+        case '25':
+                color = "#8EB1C4";
+                img = "cold.png";
+                break;
+        //Hot 36 #F56544
+        case '36':
+                color = "#F56544";
+                img = "hot.png";
+                break;
+        //Cloudy 26, 27, 44 #A4A09D
+        case '26': case '27': case '44':
+                color = "#A4A09D";
+                img = "cloudy.png";
+                break;
+        //Drizzle 8, 9 #2F431E
+        case '8': case '9':
+                color = "#506250";
+                img = "drizzle.png";
+                break;
+        //Mixed Rain Snow Sleet 5, 6, 7, 18 #859CA7
+        case '5': case '6': case '7': case '18':
+                color = "#859CA7";
+                img = "MixedRSS.png";
+                break;
+        //No asociation 0, 2, 3200
+        case '30' : 
+                color = "#ffcc00";
+                img = ""; // what is the default img?
+                break;
 
-  document.body.style.background = color;
- 
- }
+    }
+    
+    weatherGraphics.color = color;
+    weatherGraphics.img = img;
+    
+    return weatherGraphics;     
+}
+
+function changeBackground ( color ) {
+    document.body.style.background = color;
+}
 
 $(document).ready(function() {
 
@@ -162,11 +254,14 @@ $(document).ready(function() {
     location: 'Santa Coloma, Andorra',
     unit: 'f',
     success: function(weather) {
-        /*Snow 13, 14, 15, 16, 41,42, 43, 46 (creo que se deberia asociar los numeros a Snow, no un numero a la foto) */
-      html = '<h2><img class="weather-icon" src="img/weather/'+weather.code+'.png">'+weather.alt.temp+'&deg;C '+weather.currently+'</h2>';
+        weatherCode = weather.code;
+        var weatherGraphics = getWeatherGraphicsParams( weatherCode );
+        
+        /*Asociate weatherGraphics to the courrent weather */
+      html = '<h2><img class="weather-icon" src="img/weather/'+weatherGraphics.img+'">'+weather.alt.temp+'&deg;C '+weather.currently+'</h2>';
       html += '<div class="weather-region">'+weather.city+', '+weather.region+'</div>';
-      weatherCode = weather.code;
-      changeBackground( weatherCode );
+      
+      changeBackground( weatherGraphics.color );
 
       $("#weather").html(html);
     },
@@ -190,6 +285,9 @@ $(document).ready(function() {
   var database = firebase.database();
 
 });
+
+// TODO SVG hide show information of the sensors id="Sensors" object.style.display = none
+
 
 
 
