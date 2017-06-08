269f250efae3509fc2f1bada7348731fc2891737
jpnawyn <nawyn@media.mit.edu>
2017-06-08 14:41:03 -0400

    Added php calls to retrieve sensor values from PostgreSQL database. Updated sensorIds to match stored values. Adjusted behavior of some sensor types to show daily activation counts instead of last value.

--- js/date.js
+++ js/date.js
@@ -8,6 +8,11 @@
  */
 
 
+/*
+	Added Standard calls for TerMITes API Database access
+	Server root: /replace.media.mit.edu:8080/
+*/
+
 var tday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var tmonth = ['January','February','March','April','May','June','July','August','September','October','November','December']; 
 
@@ -225,23 +230,24 @@ function changeBackground ( color ) {
 // Catalog of sensors installed and their IDs in the SVG. 
 var sensorCatalog = [
     /* Presence values: 0 or 1  Sensors: AndorraPIR1 */
-    { name: "sensor-presence", id: ["pres1-value"] },
+    { name: "sensor-presence", id: ["AndorraPIR1"] },
     /* Motion values: 0.00-0.10 Do we need to scale them yes, boolean. Sensors: AndorraMotion1, AndorraMotion2 */
-    { name: "sensor-motion", id: ["mot1-value","mot2-value"] },
+    { name: "sensor-motion", id: ["AndorraMotion1","AndorraMotion2"] },
     /* Window values: 0 or 1  Sensors form left to right: AndorraMITes-0769, AndorraMITes-1012,AndorraMITes-0109, AndorraMITes-1001, AndorraMITes-333 */
     { name: "sensor-window", id: ["w0769-value", "w1012-value", "w0109-value", "w1001-value","w333-value"] },
     /* Door values: 0 or 1 close  Sensors: Reed2 */
     { name: "sensor-door", id: ["door2-value"] },
     /* Light values: 0-1250  Sensors: AndorraLight2, AndorraLight3, AndorraLight4,AndorraLight5, AndorraLight6 */
-    { name: "sensor-light", id: ["light2-value","light3-value","light4-value","light4-value","light5-value","light6-value"] },
+    { name: "sensor-light", id: ["AndorraLight2","AndorraLight3","AndorraLight4","AndorraLight5","AndorraLight6"] },
      /* Curtains values: 0 or 1 move  Sensors form left to right: AndorraMITes-0210, AndorraMITes-0995, AndorraMITes-0066,AndorraMITes-0325*/
     { name: "sensor-curtains", id: ["cur0210-value","cur0995-value","cur0066-value","cur0325-value"] },
     /* Temperature values: 0-30  Sensors: AndorraLight2, AndorraLight3, AndorraLight4,AndorraLight5, AndorraLight6 */
-    { name: "sensor-temperature", id: ["temp2-value","temp3-value","temp4-value","temp4-value","temp5-value","temp6-value"] },
+//     { name: "sensor-temperature", id: ["temp2-value","temp3-value","temp4-value","temp4-value","temp5-value","temp6-value"] },
+    { name: "sensor-temperature", id: ["AndorraLight2","AndorraLight3","AndorraLight4","AndorraLight5","AndorraLight6"] },
     /* CO2 values:  2.6-1 Inverted, volt sign Sensor: AndorraBob1 */
-    { name: "sensor-co2", id: ["co21-value"] },
+    { name: "sensor-co2", id: ["AndorraBob1"] },
     /* Humidity values:  0-100%RH Sensor: AndorraHumidity1 */
-    { name: "sensor-humidity", id: ["hum1-value"] }
+    { name: "sensor-humidity", id: ["AndorraHumidity1"] }
 ]; 
 
 // This function id blocking all the code
@@ -286,51 +292,51 @@ function showSensorValue ( typeOfSensor ) {
         switch ( typeOfSensor ) {
         
           case 'sensor-presence':
-         
-                var numberOfSensors = sensorIDs[0].id.length;
-                // TODO: change the icon depending on the value of the sensor (0,1) if value=1 display-icon-1 else display-icon-0
-                for (var i = 0; i < numberOfSensors; i++) {
-                  if ( phantom ) {
-                    console.log(sensorIDs[0].id[i]+'-icon-0');
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-0').setAttribute("display","none");
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-1').setAttribute("display","block");
-                    phantom = false;
-                    $(svgDoc.getElementById(sensorIDs[0].id[i])).text("1");
-                  }
-                  else {
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-0').setAttribute("display","block");
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-1').setAttribute("display","none");
-                    phantom = true;
-                    $(svgDoc.getElementById(sensorIDs[0].id[i])).text("0");
-                  }
-                }
-                break;
-          
-          case 'sensor-temperature':
-                // Change value for each sensor.
                 var numberOfSensors = sensorIDs[0].id.length;
                 for (var i = 0; i < numberOfSensors; i++) {
-                  $(svgDoc.getElementById(sensorIDs[0].id[i])).text("21.5°C");
+	                var sensorId = sensorIDs[0].id[i];   
+	                $.ajax({
+				        url: 'http://replace.media.mit.edu:8080/electronautes/php/getActivationCountTodayByName.php?name=' + sensorId,
+				        async: false,
+				        dataType: 'json',
+				        success: function(data) {
+					        var count = data['count'];
+							if (count > 0) {
+			                    svgDoc.getElementById(sensorId+'-icon-0').setAttribute("display","none");
+			                    svgDoc.getElementById(sensorId+'-icon-1').setAttribute("display","block");
+			                    $(svgDoc.getElementById(sensorId+'-value')).text(count.toString());
+			                }
+			                else {
+			                    svgDoc.getElementById(sensorId+'-icon-0').setAttribute("display","block");
+			                    svgDoc.getElementById(sensorId+'-icon-1').setAttribute("display","none");
+			                    $(svgDoc.getElementById(sensorId+'-value')).text("0");
+			                }
+				        }
+				    });
                 }
-                break;
-
+                break;               
           case 'sensor-motion':
                 var numberOfSensors = sensorIDs[0].id.length;
-                // TODO: change the icon depending on the value of the sensor (0,1) if value=1 display-icon-1 else display-icon-0
-                for (var i = 0; i < numberOfSensors; i++) {
-                  if ( motion ) {
-                    console.log(sensorIDs[0].id[i]+'-icon-0');
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-0').setAttribute("display","none");
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-1').setAttribute("display","block");
-                    motion = false;
-                    $(svgDoc.getElementById(sensorIDs[0].id[i])).text("1");
-                  }
-                  else {
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-0').setAttribute("display","block");
-                    svgDoc.getElementById(sensorIDs[0].id[i]+'-icon-1').setAttribute("display","none");
-                    motion = true;
-                    $(svgDoc.getElementById(sensorIDs[0].id[i])).text("0");
-                  }
+   				for (var i = 0; i < numberOfSensors; i++) {
+	            	var sensorId = sensorIDs[0].id[i];
+	                $.ajax({
+				        url: 'http://replace.media.mit.edu:8080/electronautes/php/getActivationCountTodayByName.php?name=' + sensorId,
+				        async: false,
+				        dataType: 'json',
+				        success: function(data) {
+					        var count = data['count'];
+					        if (count > 0) {
+					            svgDoc.getElementById(sensorId+'-icon-0').setAttribute("display","none");
+			                    svgDoc.getElementById(sensorId+'-icon-1').setAttribute("display","block");
+			                    $(svgDoc.getElementById(sensorId+'-value')).text(count.toString());
+							}
+							else {
+			                    svgDoc.getElementById(sensorId+'-icon-0').setAttribute("display","block");
+			                    svgDoc.getElementById(sensorId+'-icon-1').setAttribute("display","none");
+			                    $(svgDoc.getElementById(sensorId+'-value')).text("0");
+		                 	}
+		                }
+		            });
                 }
                 break;
           case 'sensor-window':
@@ -378,10 +384,19 @@ function showSensorValue ( typeOfSensor ) {
                 break;
           case 'sensor-light':
                 // Change value for each sensor.
-                var numberOfSensors = sensorIDs[0].id.length;
+				var numberOfSensors = sensorIDs[0].id.length;
                 for (var i = 0; i < numberOfSensors; i++) {
-                  $(svgDoc.getElementById(sensorIDs[0].id[i])).text("1200");
+					var sensorId = sensorIDs[0].id[i];
+					$.ajax({
+				        url: 'http://replace.media.mit.edu:8080/electronautes/php/getLastValueByName.php?name=' + sensorId,
+				        async: false,
+				        dataType: 'json',
+				        success: function(data) {
+							$(svgDoc.getElementById(sensorId + "-value")).text(data[0]['value']);	
+				        }    
+					});
                 }
+                break;   
                 break;
           case 'sensor-curtains':
                 // Change value for each sensor.
@@ -394,16 +409,47 @@ function showSensorValue ( typeOfSensor ) {
                 // Change value for each sensor. ***This value needs to be inverted volt sign
                 var numberOfSensors = sensorIDs[0].id.length;
                 for (var i = 0; i < numberOfSensors; i++) {
-                  $(svgDoc.getElementById(sensorIDs[0].id[i])).text("2.4");
-                }
+	                var sensorId = sensorIDs[0].id[i];  
+	           		$.ajax({
+				        url: 'http://replace.media.mit.edu:8080/electronautes/php/getLastValueByName.php?name=' + sensorId,
+				        async: false,
+				        dataType: 'json',
+				        success: function(data) {
+							$(svgDoc.getElementById(sensorId + "-value")).text(data[0]['value']);	
+				        }    
+					});
+				}
                 break;
           case 'sensor-humidity':
                 // Change value for each sensor. ***This value needs to be inverted volt sign
                 var numberOfSensors = sensorIDs[0].id.length;
                 for (var i = 0; i < numberOfSensors; i++) {
-                  $(svgDoc.getElementById(sensorIDs[0].id[i])).text("60%");
+					var sensorId = sensorIDs[0].id[i];
+					$.ajax({
+				        url: 'http://replace.media.mit.edu:8080/electronautes/php/getLastValueByName.php?name=' + sensorId,
+				        async: false,
+				        dataType: 'json',
+				        success: function(data) {
+							$(svgDoc.getElementById(sensorId + "-value")).text(data[0]['value'] + '%');	
+				        }    
+					});
                 }
-                break;       
+                break;
+           case 'sensor-temperature':
+                // Change value for each sensor.
+                var numberOfSensors = sensorIDs[0].id.length;
+                for (var i = 0; i < numberOfSensors; i++) {
+	                var sensorId = sensorIDs[0].id[i];                
+				    $.ajax({
+				        url: 'http://replace.media.mit.edu:8080/electronautes/php/getLastTemperatureByName.php?name=' + sensorId,
+				        async: false,
+				        dataType: 'json',
+				        success: function(data) {
+							$(svgDoc.getElementById(sensorId + "-temp")).text(data[0]['temperature']);	
+				        }
+				    });
+                }
+                break;   
         }
         
         // Display sensors on the SVG.
