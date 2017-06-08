d394f64dab6dcc0033e4b4e9f35e8fddb736d29b
jpnawyn <nawyn@media.mit.edu>
2017-06-08 14:59:22 -0400

    PHP script to query TerMITes database and retrieve a list of sensors for a specified type and project. Not currently used.

---
+++ php/getSensorTypesByProject.php
@@ -0,0 +1,25 @@
+<?php 
+header("content-type: application/json"); 
+
+include('pg_connect.php');
+
+$project = $_GET["project"];
+
+$query="SELECT DISTINCT sensortype FROM termiteinfo WHERE project LIKE '%$project%' ORDER BY sensortype";
+
+$result = pg_query($query);
+
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+$arr = pg_fetch_all($result);
+
+echo $_GET['callback'] . '(' . json_encode($arr, JSON_NUMERIC_CHECK) . ')';   
+
+include('pg_disconnect.php');
+
+?>
+
+
