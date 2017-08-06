edd2faac92d21da4d6cbb2289d66c112e23ece82
Núria Macià <macia.nuria@gmail.com>
2017-08-06 10:36:22 -0400

    Indent relate.php

--- relate.php
+++ relate.php
@@ -7,163 +7,162 @@
  Last modified: April 2017
  -->
 
-        <?php include './php/theme-functions/header.php';?>
+    <?php include './php/theme-functions/header.php';?>
 
-        <!-- MAIN -->
+    <!-- MAIN -->
 
-        <main>	
-			<div class="container no-padding">
-                <div class="block no-margin clearfix">
-                    <div class="col-xs-12 col-md-7 no-padding">
-                      
-                      <div class="r-emotions">
-                     
-                      </div>
-                      
-                      <div class="r-sensorsbox-and-dragbox">
-                        
-                          
+    <main>	
+        <div class="container no-padding">
+            <div class="block no-margin clearfix">
+                
+                <!-- Drag and drop -->
+                <div class="col-xs-12 col-md-7 no-padding">
+
+                    <div class="r-emotions">
+                    </div>
+
+                    <div class="r-sensorsbox-and-dragbox">
+
+                        <!-- Left side sensors -->
                         <div class="r-sensors-side" id="left-buttonbox-sensors" ondrop="drop(event)" ondragover="allowDrop(event)">
-                          
-                          <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-presence">
-                          
-                                          <label>
-                                              <input type="radio" name="sensor" id="presence" value="presence" />
-                                              <figure>
-                                                  <img src="./img/sensors/presence.png" alt="Presence" />
-                                                  <figcaption>Presence</figcaption>
-                                              </figure>
-                                          </label>
-                          </div>
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-motion">
-                          <label>
-                            <input type="radio" name="sensor" id="motion" value="motion" />
-                            <figure>
-                               <img src="./img/sensors/motion.png" alt="Motion" />
-                                    <figcaption>Motion</figcaption>
-                            </figure>
-                          </label>
+
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-presence">
+                                  <label>
+                                      <input type="radio" name="sensor" id="presence" value="presence" />
+                                      <figure>
+                                          <img src="./img/sensors/presence.png" alt="Presence" />
+                                          <figcaption>Presence</figcaption>
+                                      </figure>
+                                  </label>
+                            </div>
+                  
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-motion">
+                                <label>
+                                    <input type="radio" name="sensor" id="motion" value="motion" />
+                                    <figure>
+                                        <img src="./img/sensors/motion.png" alt="Motion" />
+                                        <figcaption>Motion</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+                    
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-proximity">
+                                <label>
+                                    <input type="radio" name="sensor" id="proximity" value="promixity" />
+                                    <figure>
+                                        <img src="./img/sensors/proximity.png" alt="Proximity" />
+                                        <figcaption>Proximity</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-window">
+                                <label>
+                                      <input type="radio" name="sensor" id="window" value="window" />
+                                      <figure>
+                                          <img class='img-window-sensor' src="./img/sensors/window.png" alt="Window" />
+                                          <figcaption>Window</figcaption>
+                                      </figure>
+                                </label> 
+                            </div>
+                  
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-door">
+                                <label>
+                                    <input type="radio" name="sensor" id="door" value="door" />
+                                    <figure>
+                                        <img src="./img/sensors/door.png" alt="I feel awesome." />
+                                        <figcaption>Door</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+                            
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-curtains">
+                                <label>
+                                    <input type="radio" name="sensor" id="curtains" value="curtains" />
+                                    <figure>
+                                        <img src="./img/sensors/curtains.png" alt="I feel normal." />
+                                        <figcaption>Curtains</figcaption>
+                                    </figure>
+                                </label> 
+                            </div>
+
                         </div>
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-proximity">
-                          <label>
-                                            <input type="radio" name="sensor" id="proximity" value="promixity" />
-                                            <figure>
-                                                <img src="./img/sensors/proximity.png" alt="Proximity" />
-                                                <figcaption>Proximity</figcaption>
-                                            </figure>
-                                        </label>
-                        </div>
-                          
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-window">
-                         <label>
-                                              <input type="radio" name="sensor" id="window" value="window" />
-                                              <figure>
-                                                  <img class='img-window-sensor' src="./img/sensors/window.png" alt="Window" />
-                                                  <figcaption>Window</figcaption>
-                                              </figure>
-                                          </label> 
-                        </div>
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-door">
-                          <label>
-                                            <input type="radio" name="sensor" id="door" value="door" />
-                                            <figure>
-                                                <img src="./img/sensors/door.png" alt="I feel awesome." />
-                                                <figcaption>Door</figcaption>
-                                            </figure>
-                                        </label>
-                          
-                        </div>
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-curtains">
-                         <label>
-                                            <input type="radio" name="sensor" id="curtains" value="curtains" />
-                                            <figure>
-                                                <img src="./img/sensors/curtains.png" alt="I feel normal." />
-                                                <figcaption>Curtains</figcaption>
-                                            </figure>
-                                        </label> 
-                        </div>
-                        
-                      </div>
-                      
+
+                        <!-- Dropzone -->    
                         <div class="r-dragbox" id="center-dragbox-forsensors" ondrop="drop(event)" ondragover="allowDrop(event)">
                         </div>
-                      
+
+                        <!-- Right side sensors -->
                         <div class="r-sensors-side" id="right-buttonbox-sensors" ondrop="drop(event)" ondragover="allowDrop(event)">
-                        
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-temperature">
-                          <label>
-                             <input type="radio" name="sensor" id="temparature" value="temperature" />
-                                <figure>
-                                    <img src="./img/sensors/temperature.png" alt="Temperature" />
-                                    <figcaption>Temperature</figcaption>
-                                </figure>
-                          </label>
-                          
-                        </div>
-                        
-                        <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-humidity">
-                            <label>
-                              <input type="radio" name="sensor" id="humidity" value="humidity" />
-                                <figure>
-                                  <img src="./img/sensors/humidity-2.png" alt="Humidity" />
-                                  <figcaption>Humidity</figcaption>
-                                </figure>
-                            </label>
-                          
-                        </div>
-                         <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-pressure">
-                           <label>
-                              <input type="radio" name="sensor" id="pressure" value="pressure" />
-                              <figure>
-                                  <img src="./img/sensors/pressure.png" alt="Pressure" />
-                                  <figcaption>Pressure</figcaption>
-                              </figure>
-                          </label>
-                          
-                        </div>
-                         <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-co2">
-                           <label>
-                                <input type="radio" name="sensor" id="co2" value="co2" />
-                                <figure>
-                                    <img src="./img/sensors/co2.png" alt="CO2" />
-                                    <figcaption>CO2</figcaption>
-                                </figure>
-                          </label>
-                          
-                        </div>
-                         <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-light">
-                           <label>
-                              <input type="radio" name="sensor" id="light" value="light" />
-                              <figure>
-                                  <img src="./img/sensors/light.png" alt="I feel energetic." />
-                                  <figcaption>Light</figcaption>
-                              </figure>
-                          </label>
-                          
-                        </div>
-                    
-                      </div>
-                        
-                      </div>
-                      
-                  </div>
-                      
-                    
-                      
-                    <div class="col-xs-12 col-md-5 no-padding">
-                        <div>
-                            <div class="welcome-weather-date">      
-                                <span id="getDayOfWeek"></span>
-                                <span id="getDate"></span>
-                                <div id="weather" class="weather"></div>
+
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-temperature">
+                                <label>
+                                    <input type="radio" name="sensor" id="temparature" value="temperature" />
+                                    <figure>
+                                        <img src="./img/sensors/temperature.png" alt="Temperature" />
+                                        <figcaption>Temperature</figcaption>
+                                    </figure>
+                                </label>
                             </div>
-                                                          
-                        </div>   
+
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-humidity">
+                                <label>
+                                    <input type="radio" name="sensor" id="humidity" value="humidity" />
+                                    <figure>
+                                        <img src="./img/sensors/humidity-2.png" alt="Humidity" />
+                                        <figcaption>Humidity</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+                     
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-pressure">
+                                <label>
+                                    <input type="radio" name="sensor" id="pressure" value="pressure" />
+                                    <figure>
+                                        <img src="./img/sensors/pressure.png" alt="Pressure" />
+                                        <figcaption>Pressure</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+                    
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-co2">
+                                <label>
+                                    <input type="radio" name="sensor" id="co2" value="co2" />
+                                    <figure>
+                                        <img src="./img/sensors/co2.png" alt="CO2" />
+                                        <figcaption>CO2</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+                     
+                            <div class="r-button-sensor" draggable="true" ondragstart="drag(event)" id="drag-light">
+                                <label>
+                                    <input type="radio" name="sensor" id="light" value="light" />
+                                    <figure>
+                                        <img src="./img/sensors/light.png" alt="I feel energetic." />
+                                        <figcaption>Light</figcaption>
+                                    </figure>
+                                </label>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-            </div>       
-        </main>
 
-        <?php include './php/theme-functions/footer.php';?>
-        
-    </body>    
+                <div class="col-xs-12 col-md-5 no-padding">
+                    <div>
+                        <div class="welcome-weather-date">      
+                            <span id="getDayOfWeek"></span>
+                            <span id="getDate"></span>
+                            <div id="weather" class="weather"></div>
+                        </div>
+                    </div>   
+                </div>
+           
+            </div>
+        </div>       
+    </main>
+
+    <?php include './php/theme-functions/footer.php';?>
+
+</body>    
